04148e4831da30aeeac9cd460654c7bf574205a2
FahmimaNaznin <naznin23105101248@diu.edu.bd>
2024-12-04 17:40:40 +0600

    commot

--- src/atm/simulator/system/Signup2.java
+++ src/atm/simulator/system/Signup2.java
@@ -14,3 +14,28 @@ public class Signup2 extends JFrame implements ActionListener {
     String formno;
     Signup2(String formno){
         super("APPLICATION FORM");
+       
+       
+ ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/bank.png"));
+        Image i2 = i1.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(150,5,100,100);
+        add(image);
+
+        this.formno = formno;
+
+        JLabel l1 = new JLabel("Page 2 :-");
+        l1.setFont(new Font("Raleway", Font.BOLD,22));
+        l1.setBounds(300,30,600,40);
+        add(l1);
+
+        JLabel l2 = new JLabel("Additonal Details");
+        l2.setFont(new Font("Raleway", Font.BOLD,22));
+        l2.setBounds(300,60,600,40);
+        add(l2);
+
+        JLabel l3 = new JLabel("Religion :");
+        l3.setFont(new Font("Raleway", Font.BOLD,18));
+        l3.setBounds(100,120,100,30);
+        add(l3);
