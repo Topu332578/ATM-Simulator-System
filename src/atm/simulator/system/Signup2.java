c34dbc8267051bb05d4d7ff49450283f862c0940
FahmimaNaznin <naznin23105101248@diu.edu.bd>
2024-12-04 17:38:17 +0600

    2nd

--- src/atm/simulator/system/Signup2.java
+++ src/atm/simulator/system/Signup2.java
@@ -7,4 +7,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Signup2 extends JFrame implements ActionListener {
-   
+   JComboBox comboBox,comboBox2,comboBox3,comboBox4,comboBox5;
+    JTextField textPan,textAadhar;
+    JRadioButton r1,r2, e1,e2;
+    JButton next;
+    String formno;
+    Signup2(String formno){
+        super("APPLICATION FORM");
