bfc9deda33fc0321d7f846749bd14eb7099e6d28
Thoushif Ahammed Farash Purno <purno23105101237@diu.edu.bd>
2024-12-10 15:00:08 +0600

    Update mini.java

--- src/atm/simulator/system/mini.java
+++ src/atm/simulator/system/mini.java
@@ -6,3 +6,30 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.ResultSet;
+
+public class mini extends JFrame implements ActionListener {
+    String pin;
+    JButton button;
+    mini(String pin){
+        this.pin = pin;
+        getContentPane().setBackground(new Color(255,204,204));
+        setSize(400,600);
+        setLocation(20,20);
+        setLayout(null);
+
+        JLabel label1 = new JLabel();
+        label1.setBounds(20,140,400,200);
+        add(label1);
+
+        JLabel label2 = new JLabel("TechCoder A.V");
+        label2.setFont(new Font("System", Font.BOLD,15));
+        label2.setBounds(150,20,200,20);
+        add(label2);
+
+        JLabel label3 = new JLabel();
+        label3.setBounds(20,80,300,20);
+        add(label3);
+
+        JLabel label4 = new JLabel();
+        label4.setBounds(20,400,300,20);
+        add(label4);
