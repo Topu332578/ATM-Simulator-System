e4af63de32e454965194a635e3a51800359a892a
Topu332578 <islam2305101963@diu.edu.bd>
2024-12-12 02:52:27 +0600

    Add files via upload

--- Pin.java
+++ Pin.java
@@ -4,3 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+public class Pin extends JFrame implements ActionListener {
+    JButton b1,b2;
+    JPasswordField p1,p2;
+    String pin;
