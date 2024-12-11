ba40c3730b825f54b1bee91edd964d74f943d94c
Topu332578 <islam2305101963@diu.edu.bd>
2024-12-11 13:37:05 +0600

    Add files via upload

--- Withdrawl.java
+++ Withdrawl.java
@@ -12,4 +12,10 @@ Withdrawl(String pin) {
     JLabel l3 = new JLabel(i3);
     l3.setBounds(0, 0, 1550, 830);
     add(l3);
+
+    JLabel label1 = new JLabel("MAXIMUM WITHDRAWAL IS Tk.10,000");
+    label1.setForeground(Color.WHITE);
+    label1.setFont(new Font("System", Font.BOLD, 16));
+    label1.setBounds(460,180,700,35);
+    l3.add(label1);
 }
