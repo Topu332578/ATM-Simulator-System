8aa1b1f428710cccc46587126be410b51c45baad
Topu332578 <islam2305101963@diu.edu.bd>
2024-12-11 13:33:14 +0600

    Add files via upload

--- Withdrawl.java
+++ Withdrawl.java
@@ -4,3 +4,12 @@ public class Withdrawl {
 
     JButton b1, b2;
 }
+Withdrawl(String pin) {
+    this.pin = pin;
+    ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/atm2.png"));
+    Image i2 = i1.getImage().getScaledInstance(1550, 830, Image.SCALE_DEFAULT);
+    ImageIcon i3 = new ImageIcon(i2);
+    JLabel l3 = new JLabel(i3);
+    l3.setBounds(0, 0, 1550, 830);
+    add(l3);
+}
