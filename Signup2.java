57c189c602d9516cd50130eab42a21ce882bcc7f
FahmimaNaznin <naznin23105101248@diu.edu.bd>
2024-12-11 23:04:07 +0600

    Update Signup2.java

--- Signup2.java
+++ Signup2.java
@@ -1,18 +1,1 @@
-package atm.simulator.system;
 
-
-import javax.swing.*;
-
-public class Signup2 extends JFrame {
-
-    Signup2(){
-        setSize(500,500);
-        setVisible(true);
-
-    }
-
-    public static void main(String[] args) {
-        new Signup2();
-
-    }
-}
